a3f4a50c527015b367221cc61fa89f0b870c412b
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2026-03-11 07:40:12 +0900

    fix: gradle9.4から必須になったアノテーションを付加
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- plugin-build/plugin/src/main/kotlin/jp/co/gahojin/refreshVersions/RefreshVersionsCleanupTask.kt
+++ plugin-build/plugin/src/main/kotlin/jp/co/gahojin/refreshVersions/RefreshVersionsCleanupTask.kt
@@ -11,28 +11,35 @@ import jp.co.gahojin.refreshVersions.internal.VersionCatalogCleaner
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 
+@CacheableTask
 abstract class RefreshVersionsCleanupTask : DefaultTask() {
     init {
         group = Constants.GROUP
     }
 
     @get:InputFile
+    @get:PathSensitive(PathSensitivity.ABSOLUTE)
     abstract val versionsTomlFile: Property<File>
 
     @get:Internal
     abstract val sortSection: Property<Boolean>
 
     @get:InputFile
+    @get:PathSensitive(PathSensitivity.ABSOLUTE)
     @get:Optional
     abstract val rootSettingsFile: Property<File>
 
     @get:InputFile
+    @get:PathSensitive(PathSensitivity.ABSOLUTE)
     @get:Optional
     abstract val buildSrcSettingsFile: Property<File>
 
--- plugin-build/plugin/src/main/kotlin/jp/co/gahojin/refreshVersions/RefreshVersionsTask.kt
+++ plugin-build/plugin/src/main/kotlin/jp/co/gahojin/refreshVersions/RefreshVersionsTask.kt
@@ -17,25 +17,32 @@ import kotlinx.coroutines.runBlocking
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 
+@CacheableTask
 abstract class RefreshVersionsTask : DefaultTask() {
     init {
         group = Constants.GROUP
     }
 
     @get:InputFile
+    @get:PathSensitive(PathSensitivity.ABSOLUTE)
     abstract val versionsTomlFile: Property<File>
 
     @get:InputFile
+    @get:PathSensitive(PathSensitivity.ABSOLUTE)
     @get:Optional
     abstract val rootSettingsFile: Property<File>
 
     @get:InputFile
+    @get:PathSensitive(PathSensitivity.ABSOLUTE)
     @get:Optional
     abstract val buildSrcSettingsFile: Property<File>
 
